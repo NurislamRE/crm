6f6b20f765e4236fe7c4f001551bbc88b3e4b7df
Нурислам <nurislam.raushanbek@gmail.com>
2022-04-25 17:51:11 +0600

    add crm

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const discountCheckbox = document.querySelector('.modal__checkbox');
+const discountInput = document.querySelector('.modal__input_discount');
+
+console.log(modalTitle);
+console.log(modalForm);
+console.log(discountCheckbox);
+console.log(discountInput);
